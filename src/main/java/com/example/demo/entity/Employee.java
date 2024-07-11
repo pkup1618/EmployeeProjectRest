6e254245dcc1db324cee0ec30fe981958e0cbd31
Pkup1618 <andrey0sintsev@mail.ru>
2024-07-11 09:52:41 +0500

    Создание шаблона проекта

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+public class Employee {
+    int id;
+    String name;
+    int bossId;
+}
---
+++ src/main/java/com/example/demo/entity/Metric.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+public class Metric {
+    String name;
+    int employeeId;
+    int value; // constraints 0-100
+}
